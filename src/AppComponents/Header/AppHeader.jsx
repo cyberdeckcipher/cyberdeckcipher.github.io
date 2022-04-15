ffa88ca5737dad10e73f1340326bc2b6c9a3de14
Ulisses <ulisses.sigma@gmail.com>
2022-04-15 11:02:32 -0300

    :sparkles: adding Breadcrumbs at the header.

--- src/AppComponents/Header/AppHeader.jsx
+++ src/AppComponents/Header/AppHeader.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Button from "../../Components/Button/Button";
 import styles from '../../Styles/App.module.css';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import Pallete from '../../Styles/Pallete.json';
 import './AppHeader.css';
 
 const RenderButton = (text) =>{
@@ -11,11 +12,63 @@ const RenderButton = (text) =>{
             text = {text}
             onclick = {
                 async (event)=>{
-                event.preventDefault();
-                navigate(("../"+text), { replace: true });
+                    event.preventDefault();
+                    navigate(('../'+text), { replace: true });
                 }
             } 
         />
+    )
+}
+
+const isLastcrumb = (index, size) =>{
+    return ((size === 0) || ((index+1) >= size))
+}
+
+const GetBreadcrumbStyle = (lastcrumb) => {
+    if(lastcrumb){
+        return {
+            color: Pallete.hightlight
+        }
+    }
+}
+
+const RenderBreadcrumbButton = () => {
+    let navigate = useNavigate();
+    let location = useLocation();
+    let {pathname} = location;
+    var pathnames = pathname.split('/').filter(x => x);
+    return(
+        <div className='AppHeaderBreadcrumb'>
+            <span onClick={ 
+                async (event)=>{
+                event.preventDefault();
+                navigate(('/'), { replace: true });
+            }}>
+                Home
+            </span>
+            {
+                pathnames.map((name, index) =>{
+                    var lastcrumb = isLastcrumb(index, pathnames.length);
+                    return(
+                        <span key = {index}>
+                            {' > '}
+                            <span 
+                                style={GetBreadcrumbStyle(lastcrumb)}
+                                onClick={ async (event)=>{
+                                    event.preventDefault();
+                                    if(!lastcrumb){
+                                        var goTo = pathnames.slice(0,index+1).join('/');
+                                        navigate((goTo), { replace: true });
+                                    }
+                                }}
+                            >
+                                {name}
+                            </span>
+                        </span>
+                    )
+                })
+            }
+        </div>
     )
 }
 
@@ -26,9 +79,7 @@ const AppHeader = () => {
                 {RenderButton('Projects')}
                 {RenderButton('Arts')}
             </div>
-            <div className='AppHeaderBreadcrumb'>
-                <span>Breadcrumbs {'>'} here</span>
-            </div>
+            <RenderBreadcrumbButton/>
         </div>
     )
 }
