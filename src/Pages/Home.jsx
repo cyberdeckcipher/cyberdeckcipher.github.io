ec061705345d3ba3b4ccb98db0e48ff36eda8c9e
Ulisses <ulisses.sigma@gmail.com>
2022-04-25 20:21:23 -0300

    :hammer: adding sadguy to the homepage to try to force it to deploy.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import RotatingCube from '../Components/RotatingCube/RotatingCube';
 import pageStyle from '../Styles/Pages.module.css';
+import SadGuy from '../Images/SadGuy/SadGuy.gif';
 
 const Home = () => {
     return (
         <>
             <RotatingCube/>
-            <div className={pageStyle.RightHalf}><span>Home page.</span></div>
+            <div className={pageStyle.RightHalf}><img src={SadGuy} alt='404 Page not found' /></div>
         </>
     )
 }
